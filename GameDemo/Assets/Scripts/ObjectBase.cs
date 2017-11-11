1b08b381c8e9b8d4b7516470935e1e8b390e8548
cgi4u <cgi4u3@gmail.com>
2017-11-11 11:39:20 +0900

    initial commit

---
+++ GameDemo/Assets/Scripts/ObjectBase.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//모든 개체의 공통기능 및 속성 정의
+public class ObjectBase : MonoBehaviour {
+	protected enum Behaviour { move, attack, idle };
+
+	protected GameObject enemy;
+	protected DetectorBehaviour detector;
+	protected Behaviour curBehavior;
+	protected Behaviour beforeBehavior; 
+	protected string[] animationList;
+
+	public float attackFreq;
+	protected float timeSpent;
+	//NavMeshAgent agent;
+
+	protected void Start () {
+		enemy = null;
+		detector = transform.GetChild(0).gameObject.GetComponent<DetectorBehaviour>();
+														//탐지 콜라이더
+
+		timeSpent = attackFreq;							//처음 적 인식시 바로 공격
+	}
+
+	public int HP;
+	public int ATK;
+
+	//데미지 입음
+	public void damaged (int dmg){
+		HP -= dmg;
+		Debug.Log (dmg + " point damaged to" + enemy);
+		if (HP <= 0) {
+			Destroy (gameObject);
+			Debug.Log ("destroyed!");
+		}
+	}
+
+
+	/* Legacy Code
+	//적 인식 시 공격 상태로 변경
+	public void changeToAttack(GameObject _enemy){
+		enemy = _enemy;
+		beforeBehavior = curBehavior;
+		curBehavior = Behaviour.attack;
+	}
+	*/
+}
---
+++ GameDemo/Assets/Scripts/UnitGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//유닛 생성기의 행동 정의
+public class UnitGenerator : MonoBehaviour {
+	public GameObject unit;
+	public Transform waypoint;
+
+	private float timeSpent;
+	float checkTime;
+	//int unitNumber = 0;
+
+	void Start () {
+		timeSpent = 0.0f;
+		checkTime = 1.0f;
+	}
+	
+	//일정 시간마다 할당된 유닛 생성
+	void Update () {
+		GameObject newUnit;
+
+		timeSpent += Time.deltaTime;
+		if (timeSpent > checkTime) {
+			newUnit = Instantiate (unit);
+			newUnit.transform.position = transform.position;
+			newUnit.tag = string.Copy (gameObject.tag);
+			newUnit.GetComponent<UnitBehaviour> ().waypoint = waypoint;
+
+			//unitNumber += 1;
+			timeSpent = 0.0f;
+		}
+	}
+}
