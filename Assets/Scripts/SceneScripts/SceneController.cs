cb45f97f2b4dc68afd268afc0ae1e0688ef376a3
cgi4u <cgi4u3@gmail.com>
2017-11-19 16:41:18 +0900

    Escape interation for Android

---
+++ Assets/Scripts/SceneScripts/SceneController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+	public string escapeScene;
+	
+	void Update () {
+		if (Application.platform == RuntimePlatform.Android) {
+			if (Input.GetKey (KeyCode.Escape)) {
+				if (escapeScene.CompareTo ("Exit") == 0) {
+					Application.Quit ();
+				}
+				else{
+					SceneManager.LoadScene(escapeScene);
+				}
+			}
+		}
+	}
+}
